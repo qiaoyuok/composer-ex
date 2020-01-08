146b3055ceaf25bfe200cdc5bfbfb49877b48ee4
Your Name <you@example.com>
2020-01-08 11:16:33 +0800

    初始化

---
+++ src/Dd.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ ************************************
+ *           Author: sunqiaoyu      *
+ *         Date: 2020-01-08 11:13   *
+ * **********************************
+ **/
+namespace Sqy\dump;
+class Dd
+{
+    public function dd($data = [])
+    {
+        return var_dump($data);
+    }
+}
